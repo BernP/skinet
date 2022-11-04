cee13cd297718c1828c70df79ec278bfb19a01b9
Your Name <you@example.com>
2022-11-04 15:30:05 -0300

    End sec 6

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -15,5 +15,7 @@ namespace Core.Interfaces
         Task<T> GetEntityWhithSpec(ISpecification<T> spec);
 
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+
+        Task<int> CountAsync(ISpecification<T> spex);
     }
 }
